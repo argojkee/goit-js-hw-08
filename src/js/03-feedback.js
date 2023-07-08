55dc6357497f35147ab1ac71902f2a8851af5c17
argojkee <maksymhaidabura@gmail.com>
2023-07-09 00:41:14 +0300

    add third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+var throttle = require('lodash.throttle');
 
+const formEl = document.querySelector('.feedback-form');
+const emailEl = formEl.querySelector('[name="email"]');
+const messageEl = formEl.querySelector('[name="message');
+
+const USER_FEEDBACK_STORAGE = 'feedback-form-state';
+let userFeedback = {};
+
+checkedLocalStorage();
+
+formEl.addEventListener('input', throttle(onChangeInput, 1000));
+formEl.addEventListener('submit', throttle(onSubmitForm, 1000));
+
+function onChangeInput(e) {
+  userFeedback[e.target.name] = e.target.value;
+  console.log('ololo');
+  localStorage.setItem(USER_FEEDBACK_STORAGE, JSON.stringify(userFeedback));
+}
+
+function checkedLocalStorage() {
+  if (localStorage.getItem(USER_FEEDBACK_STORAGE)) {
+    userFeedback = JSON.parse(localStorage.getItem(USER_FEEDBACK_STORAGE));
+
+    emailEl.value = userFeedback.email;
+    messageEl.value = userFeedback.message;
+  }
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  formEl.reset();
+  localStorage.removeItem(USER_FEEDBACK_STORAGE);
+
+  console.log(userFeedback);
+}
